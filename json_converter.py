bfc38fef80b37a3b66e9049dd6eeb0273e629bf8
Jake Schneider <jake.w.schneider@gmail.com>
2019-01-10 18:00:18 -0500

    addded web scraper stuff

---
+++ json_converter.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+import time
+from datetime import datetime
+
+#location of the data files
+path = "~/Desktop/Code4Policy/Assignments/b1-economic-policy"
+
+#function
+df = pd.read_json()	
+df.to_csv()
+	return df
+print("Saved")
+
+
+
